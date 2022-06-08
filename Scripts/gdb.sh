2fdf68c9646903b58a6be792f2447a55fbd3bfc7
Poseydon42 <vvmposeydon@gmail.com>
2022-06-08 18:50:32 +0100

    Scripts: fixed gdb.sh loading kernel image from wrong place

--- Scripts/gdb.sh
+++ Scripts/gdb.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 dir_path=$(dirname $(realpath $0))
 
-gdb ${dir_path}/../Build/Kernel/Kernel.bin -x ${dir_path}/gdb.cfg -tui
+gdb ${dir_path}/../Build/System/Kernel/Kernel.bin -x ${dir_path}/gdb.cfg -tui
+
