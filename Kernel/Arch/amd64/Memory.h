c9fc0742d1eebb7dd85d50c84c3aeb39df16d4f3
Poseydon42 <vvmposeydon@gmail.com>
2022-06-08 21:03:52 +0100

    Kernel: added physical address and size typedefs as well as defined page size on AMD64

--- Kernel/Arch/amd64/Memory.h
+++ Kernel/Arch/amd64/Memory.h
@@ -3,6 +3,13 @@
 #include <stdint.h>
 
 #define KERNEL_OFFSET 0xFFFFFF8000000000
+#define PAGE_SIZE 4096
 
 #define V2P(a) ((uintptr_t)(a) & ~KERNEL_OFFSET)
 #define P2V(a) ((uintptr_t)(a) | KERNEL_OFFSET)
+
+namespace Kernel
+{
+    using PhysicalAddress = uint64_t;
+    using PhysicalSize = uint64_t;
+} // namespace Kernel
