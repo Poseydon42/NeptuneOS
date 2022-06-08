49dd37d0abbad6acf87b45990810f9595e96863f
Poseydon42 <vvmposeydon@gmail.com>
2022-06-08 20:49:57 +0100

    Kernel: added Start and EndOfKernelMemory values

--- Kernel/Arch/Memory.h
+++ Kernel/Arch/Memory.h
@@ -1,7 +1,13 @@
 #pragma once
 
-#include <stddef.h>
+#include <cstddef>
 
 #ifdef SERENITY_AMD64
-#include <Arch/amd64/Memory.h>
+#    include <Arch/amd64/Memory.h>
 #endif
+
+namespace Kernel
+{
+    extern "C" void* StartOfKernelMemory;
+    extern "C" void* EndOfKernelMemory;
+} // namespace Kernel
